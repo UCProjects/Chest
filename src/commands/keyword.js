8521014f9f2ccf2ec6328387da52579135f3c616
feildmaster <feildmaster@users.noreply.github.com>
2021-01-21 16:30:37 -0600

    feat: keyword lookup

---
+++ src/commands/keyword.js
@@ -0,0 +1,41 @@
+const Command = require('chat-commands/src/command');
+const { events, translate } = require('../lang');
+const { simpleMode } = require('../lang/extend');
+
+const cache = new Map();
+const prefix = 'kw-'
+
+events.on('load', (data) => {
+  cache.clear();
+  simpleMode();
+  Object.keys(data).forEach((key) => {
+    if (!key.startsWith(prefix) || key.endsWith('-desc')) return;
+    cache.set(key, {
+      name: translate(key),
+      description: translate(`${key}-desc`),
+    });
+  });
+});
+
+function handler(msg, args = [], flags = {}) {
+  const needle = args.join('-').toLowerCase();
+  if (!needle) return '* No keyword provided';
+  const { name, description } = cache.get(`${prefix}${needle}`) || {};
+  if (!name) return `* ${args.join(' ')} not found`;
+  return {
+    embed: {
+      title: name,
+      description,
+    },
+  };
+}
+
+module.exports = new Command({
+  title: '',
+  alias: ['keyword', 'kw', 'effect'],
+  examples: [],
+  usage: [],
+  description: 'Get the description of a keyword',
+  flags: [],
+  handler,
+});
