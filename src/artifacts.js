20e7eb3bb2650fc535da74e5e71bf47afa9808e8
feildmaster <feildmaster@users.noreply.github.com>
2021-01-24 01:01:39 -0600

    feat: artifact command

---
+++ src/artifacts.js
@@ -0,0 +1,32 @@
+const undercards = require('./undercards');
+const login = require('./util/login');
+
+const day = 24 * 60 * 1000;
+let next = Date.now();
+
+const artifacts = new Map([['', {}]]);
+
+function fetch() {
+  const password = process.env.UC_LOGIN;
+  if (!password) return Promise.reject('No login');
+  if (Date.now() < next) return Promise.resolve();
+  return login(password)
+    .then(({ headers }) => {
+      const Cookie = headers['set-cookie'].map(cookie => cookie.split(";")[0]).join("; ") + ";";
+
+      return undercards.get('/DecksConfig', { headers: { Cookie } })
+        .then(({ data }) => {
+          if (!data) throw new Error('Missing artifact data');
+          artifacts.clear();
+          next = Date.now() + day;
+
+          JSON.parse(data.allArtifacts)
+            .forEach(artifact => artifacts.set(artifact.id, artifact));
+        });
+    });
+}
+
+module.exports = {
+  fetch,
+  artifacts,
+};
---
+++ src/commands/artifact.js
@@ -0,0 +1,61 @@
+const { artifacts, fetch } = require('../artifacts');
+const Command = require('chat-commands/src/command');
+const { translate } = require('../lang');
+const { simpleMode } = require('../lang/extend');
+
+function handler(msg, args = [], flags = {}) {
+  return fetch().catch((e) => {
+    console.error(e);
+    return '* Error retreiving artifacts';
+  }).then((error) => {
+    if (error) return error;
+    simpleMode();
+    const needle = args.join(' ').toLowerCase();
+    if (!needle) return {
+      embed: {
+        title: 'Artifacts',
+        description: [...artifacts.values()]
+          .map(({ id }) => translate(`artifact-name-${id}`))
+          .join(', '),
+      }
+    };
+    const { id, cost, legendary, image, unavailable } = [...artifacts.values()]
+      .find(({ name }) => name.toLowerCase() === needle) || {};
+    if (!id) return `* ${args.join(' ')} not found`;
+    const fields = [{
+      name: 'Type',
+      value: translate(`artifacts-${legendary ? 'legendary' : 'normal'}`),
+      inline: true,
+    }];
+    if (!unavailable) {
+      fields.push({
+        name: 'Cost',
+        value: `${cost} G`,
+        inline: true,
+      });
+    }
+    return {
+      embed: {
+        title: translate(`artifact-name-${id}`),
+        description: translate(`artifact-${id}`),
+        color: legendary ? 16766720 : 15987699,
+        fields,
+        thumbnail: {
+          url: `https://undercards.net/images/artifacts/${image}.png`,
+          width: 32,
+          height: 32,
+        },
+      },
+    };
+  });
+}
+
+module.exports = new Command({
+  title: '',
+  alias: ['artifact', 'art'],
+  examples: [],
+  usage: [],
+  description: 'Look up artifact data',
+  flags: [],
+  handler,
+});
