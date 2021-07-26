1ee30663eda01df6a6a5d49210878e9dc8f04517
feildmaster <feildmaster@users.noreply.github.com>
2021-07-26 04:13:05 -0500

    fix: safe length

--- src/util/safeLength.js
+++ src/util/safeLength.js
@@ -2,7 +2,7 @@ function getSafeLength(text = '', length = 2048, splitter = ',') {
   const len = text.length;
   if (len > length) {
     const pointer = text.lastIndexOf(splitter, length + 1);
-    return pointer > 0 ? pointer - 1 : length;
+    return pointer > 0 && pointer < length ? pointer : length;
   }
   return len;
 }
